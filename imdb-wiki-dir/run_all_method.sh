7b1b53631270b1489c218421503e81d78d5e9fb5
ruizhipu <18651885620@163.com>
2024-04-14 01:14:04 -0400

    FDS

--- imdb-wiki-dir/run_all_method.sh
+++ imdb-wiki-dir/run_all_method.sh
@@ -6,7 +6,7 @@
 #python train.py  --batch_size 64 --lr 2.5e-4 --reweight sqrt_inv --regularization_weight=100.0 --interpolation_lambda=2.0 --weight_norm
 #echo 'RankSim and square-root frequency inverse'
 #echo 'RankSim and LDS'
-python train.py --batch_size 64 --lr 2.5e-4 --reweight sqrt_inv --lds --lds_kernel gaussian --lds_ks 5 --lds_sigma 2 --regularization_weight=100.0 --interpolation_lambda=2.0 --weight_norm
-echo 'RankSim and LDS'
-#python train.py --batch_size 64 --lr 2.5e-4 --reweight sqrt_inv --lds --lds_kernel gaussian --lds_ks 5 --lds_sigma 2 --fds --fds_kernel gaussian --fds_ks 5 --fds_sigma 2 --regularization_weight=100.0 --interpolation_lambda=2.0 --weight_norm
-#echo 'RankSim and LDS + FDS'
+#python train.py --batch_size 64 --lr 2.5e-4 --reweight sqrt_inv --lds --lds_kernel gaussian --lds_ks 5 --lds_sigma 2 --regularization_weight=100.0 --interpolation_lambda=2.0 --weight_norm
+#echo 'RankSim and LDS'
+python train.py --batch_size 64 --lr 2.5e-4 --reweight sqrt_inv --lds --lds_kernel gaussian --lds_ks 5 --lds_sigma 2 --fds --fds_kernel gaussian --fds_ks 5 --fds_sigma 2 --regularization_weight=100.0 --interpolation_lambda=2.0 --weight_norm
+echo 'RankSim and LDS + FDS'
