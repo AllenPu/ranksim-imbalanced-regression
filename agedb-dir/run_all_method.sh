788f8566108e1503b825b44d6c6a1242d6078d7f
ruizhipu <18651885620@163.com>
2024-04-23 00:57:11 -0400

    test ranksim with norms

--- agedb-dir/run_all_method.sh
+++ agedb-dir/run_all_method.sh
@@ -9,8 +9,8 @@
 #python train.py --batch_size 64 --lr 2.5e-4 --reweight sqrt_inv --lds --lds_kernel gaussian --lds_ks 5 --lds_sigma 2 --fds --fds_kernel gaussian --fds_ks 5 --fds_sigma 2 --regularization_weight=100.0 --interpolation_lambda=2.0 --weight_norm
 #echo 'RankSim and LDS + FDS + SQRT weight norm'
 python train.py --batch_size 256 --lr 2.5e-4 --regularization_weight=100.0 --interpolation_lambda=2.0 --norm
-echo ' RankSim and norm'
-python train.py --batch_size 256 --lr 2.5e-4 --regularization_weight=100.0 --interpolation_lambda=2.0 --weight_norm
-echo ' RankSim and weight norm' 
-python train.py --batch_size 256 --lr 2.5e-4 --regularization_weight=100.0 --interpolation_lambda=2.0 --weight_norm --norm
-echo ' RankSim and weight norm and feature norm' 
+echo ' RankSim and norm' # tmux 3
+#python train.py --batch_size 256 --lr 2.5e-4 --regularization_weight=100.0 --interpolation_lambda=2.0 --weight_norm
+#echo ' RankSim and weight norm'  # tmux 4
+#python train.py --batch_size 256 --lr 2.5e-4 --regularization_weight=100.0 --interpolation_lambda=2.0 --weight_norm --norm
+#echo ' RankSim and weight norm and feature norm'  #tmux 5
