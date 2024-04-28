7132182970df12b4c3aca176bdf264488b449377
AllenPu <18651885620@163.com>
2024-04-28 17:17:56 -0400

    add the shot count

--- agedb-dir/utils.py
+++ agedb-dir/utils.py
@@ -161,12 +161,22 @@ def shot_reg(label, pred, maj, med, min):
     pred_dict = {'maj':0, 'med':0, 'min':0}
     # how many preditions from min to med, min to maj, med to maj, min to med
     pred_label_dict = {'min to med':0, 'min to maj':0, 'med to maj':0, 'med to min':0, 'maj to min':0, 'maj to med':0}
+    #
+    pred = pred - torch.floor(pred)
+    zero = torch.zeros_like(pred)
+    one = torch.ones_like(pred)
+    diff = pred - torch.floor(pred)
+    diff = torch.where(diff > 0.5, one, diff)
+    diff = torch.where(diff < 0.5, zero, diff)
+    pred = torch.floor(pred) + diff
+    #
     labels, preds = np.stack(label), np.floor(np.hstack(pred))
     #dis = np.floor(np.abs(labels - preds)).tolist()
     bsz = labels.shape[0]
+    #    
     for i in range(bsz):
-        k_pred = check_shot(preds[i],maj, med, min)
-        k_label = check_shot(labels[i],maj, med, min)
+        k_pred = check_shot(preds[i], maj, med, min)
+        k_label = check_shot(labels[i], maj, med, min)
         if k_pred in pred_dict.keys():
             pred_dict[k_pred] = pred_dict[k_pred] + 1
         pred_shift = check_pred_shift(k_pred, k_label)
